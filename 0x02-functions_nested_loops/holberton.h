7e2f074f1c2292b8b7c67d299534792c56e05325
eliasslousghiri <ilyasslousghiri@gmail.com>
2023-03-22 14:14:35 -0700

    ds

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,18 @@
+#ifndef  HOLBERTON_H
+#define HOLBERTON_H
+
+char _putchar(char c);
+void print_alphabet(void);
+void print alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c) ;
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int nid);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+void print_to_98(int m);
+void print_times_table(int n);
+
+#endif /* HOLBERTON_H */
