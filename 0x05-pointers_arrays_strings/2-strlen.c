787b712f79d8e0f22b38c698198af3dd622d1a8f
eliasslousghiri <ilyasslousghiri@gmail.com>
2023-03-27 11:12:43 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,22 @@
 #include "main.h"
+
+/**
+ * _strlen - Entry point
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Returns: the length of the string
+ */
 int _strlen(char *s)
 {
 	int i;
-	int count = 0;
 
-	while (*s != '\0'; i++);
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		count++;
+		i++;
 	}
-	return (count);
+
+	return (i);
 }
